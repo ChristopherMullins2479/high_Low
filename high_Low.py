f61f52b1092320e80eee286653915b6419df7f3b
christopher mullins <christophermullins831@gmail.com>
2021-05-27 19:49:53 +0100

    added the player deck gen

--- high_Low.py
+++ high_Low.py
@@ -8,12 +8,17 @@ import random
 #variables
 counter = 0
 game_Round = 0
+player_Deck_Size = 15
 deck_Hearts = ["AH","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH",]
 deck_Diamonds = ["AD","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD",]
 deck_Clubs = ["AC","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC",]
 deck_Spades = ["AS","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS",]
 
+#will store the current player deck
+player_Deck= [None]*15
+
 #functions
+
 #rules function
 def rules_Function():
     print("\n-------------------Rules-------------------")
@@ -25,30 +30,38 @@ def rules_Function():
 def game_Menue_Screen_Function(game_Round):
     print("\n-----Welcome To High Low-----")
     print("Round : "+str(game_Round))
+    print("Your Deck")
+    print(player_Deck)
 
 #new game function
 def new_Game_Function(game_Round):
     game_Round = 1
 
 def gen_Player_Deck():
-    deck_choser = (random.randint(0,4))
-    if(deck_choser == 1):
-        print("hearts")
-        card_Choser = (random.randint(-1,12))
-        print(deck_Hearts[card_Choser])
-    if(deck_choser == 2):
-        print("diamonds")
-        card_Choser = (random.randint(-1,12))
-        print(deck_Diamonds[card_Choser])
-    if(deck_choser == 3):
-        print("clubs")
-        card_Choser = (random.randint(-1,12))
-        print(deck_Clubs[card_Choser])
-    if(deck_choser == 4):
-        print("spades")
-        card_Choser = (random.randint(-1,12))
-        print(deck_Spades[card_Choser])
-
+    i = 0
+    while(i < player_Deck_Size):
+        deck_choser = (random.randint(0,4))
+        if(deck_choser == 1):
+           # print("hearts")
+            card_Choser = (random.randint(-1,12))
+            player_Deck[i]=deck_Hearts[card_Choser]
+            i = i + 1
+        if(deck_choser == 2):
+            #print("diamonds")
+            card_Choser = (random.randint(-1,12))
+            player_Deck[i]=deck_Diamonds[card_Choser]
+            i = i + 1
+        if(deck_choser == 3):
+           # print("clubs")
+            card_Choser = (random.randint(-1,12))
+            player_Deck[i]=deck_Clubs[card_Choser]
+            i = i + 1
+        if(deck_choser == 4):
+            #print("spades")
+            card_Choser = (random.randint(-1,12))
+            player_Deck[i]=deck_Spades[card_Choser]
+            i = i + 1
+    #print(player_Deck)
 
 #Depending on user choice will run different blocks of code
 while(counter != -1): 
@@ -65,8 +78,8 @@ while(counter != -1):
     if user_Main_Menue_Choice == 1:
         game_Round = game_Round + 1
         new_Game_Function(game_Round)
-        game_Menue_Screen_Function(game_Round)
         gen_Player_Deck()
+        game_Menue_Screen_Function(game_Round)
         break
 
     #runs when user choses 2
